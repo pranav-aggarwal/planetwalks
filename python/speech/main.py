d24414c9b27eaf1157b5d5541cab6d4291e5a5eb
aditya tomar <adityatomar2402@gmail.com>
2022-06-21 15:46:57 +0530

    spectrogram.py

--- python/speech/main.py
+++ python/speech/main.py
@@ -1,7 +1,17 @@
 import speech_recognition as sr
+import os
+import matplotlib.pyplot as plt
+
+#for loading and visualizing audio files
+import librosa
+import librosa.display
+
+#to play audio
+import IPython.display as ipd
 r1=sr.Recognizer()
 with sr.Microphone() as mp:
     print("speak now")
     audio =r1.listen(mp)
+librosa.display.waveplot(audio)
 get =r1.recognize_google(audio)
 print(get)
--- python/speech/spectrogram.py
+++ python/speech/spectrogram.py
@@ -1,11 +1,9 @@
 import sounddevice
 from scipy.io.wavfile import write
-
 fps=44100
-duration=1
+duration=2
 print("record....")
 recording=sounddevice.rec(int(duration*fps),samplerate=fps,channels=2)
 sounddevice.wait()
 print("done!")
-
 write("output.wav",fps,recording)
