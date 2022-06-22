26b2138f86cdc93ff248c3e877b19f5447e7b3f2
Pranav Aggarwal <paggarwal@mast.com>
2022-06-22 09:15:11 +0530

    code refactor to fetch recordings and respective spectrogram

--- python/speech/spectrogram.py
+++ python/speech/spectrogram.py
@@ -6,24 +6,56 @@ import librosa.display
 import IPython.display as ipd
 import numpy as np
 import matplotlib.pyplot as plt
-fps=44100
-duration=2
+
+import os
+import time
+
+FPS=4076
+DURATION=2
+
+# Get number of input channels using python3 -m sounddevice 
+INPUT_CHANNELS = 1
+
+OUTPUT_DIR = "recordings/"
+
+soundSample = input("Give name of sample recording, say if recoridng hello, then name would be hello\n")
+
+if soundSample == "":
+    soundSample = "random"
+    
+path = OUTPUT_DIR+soundSample+'/'
+os.makedirs(path, exist_ok=True)
+
+timestamp = time.time()
 print("record....")
-recording=sounddevice.rec(int(duration*fps),samplerate=fps,channels=2)
+recording=sounddevice.rec(int(DURATION*FPS),samplerate=FPS,channels=INPUT_CHANNELS)
 sounddevice.wait()
 print("done!")
-write("output.wav",fps,recording)
-scale_file = "C:\Users\adi61\planetwalks\python\speech\output.wav"
-ipd.Audio(scale_file)
+currentRecordingName = f'{soundSample}-{timestamp}'
+currentRecordingNameWithExtension = f'{currentRecordingName}.wav'
+recordingPath = f'{path}{currentRecordingNameWithExtension}'
+write(recordingPath, FPS,recording)
+
+
+#---------------------------------------------------------------------------
+#Loading recorded file for spectrogram 
+
+ipd.Audio(recordingPath)
 # load audio files with librosa
-scale, sr = librosa.load(scale_file)
-FRAME_SIZE = 2048
-HOP_SIZE = 512
+scale, sr = librosa.load(recordingPath)
+
+
+
+FRAME_SIZE = 2048     #Define Frame Size
+HOP_SIZE = 512        #Define Hop Size
+
 S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
 S_scale.shape
 type(S_scale[0][0])
 Y_scale = np.abs(S_scale) ** 2
-Y_scale.shape
+
+spectrogramPath = f'{path}{currentRecordingName}.csv'
+np.savetxt(spectrogramPath,Y_scale, delimiter=",")
 type(Y_scale[0][0])
 def plot_spectrogram(Y, sr, hop_length, y_axis="linear"):
     plt.figure(figsize=(25, 10))
@@ -33,6 +65,6 @@ def plot_spectrogram(Y, sr, hop_length, y_axis="linear"):
                              x_axis="time",
                              y_axis=y_axis)
     plt.colorbar(format="%+2.f")
-    Y_log_scale = librosa.power_to_db(Y_scale)
+Y_log_scale = librosa.power_to_db(Y_scale)
 plot_spectrogram(Y_log_scale, sr, HOP_SIZE)
 plot_spectrogram(Y_log_scale, sr, HOP_SIZE, y_axis="log")
